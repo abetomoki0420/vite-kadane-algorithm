7167ab15366cff3d1b353cdd47942c01ec299ce1
abetomoki0420 <kimi.jack.of.all.trades@gmail.com>
2021-10-31 08:08:00 +0900

    feat: add increment and decrement buttons

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,26 @@ import useKadane from "./composables/useKadane"
 
 const size = ref(1)
 
+const sizeIncrement = () => {
+  if (!isIncrementable(size.value)) return
+
+  size.value++
+}
+
+const sizeDecrement = () => {
+  if (!isDecrementable(size.value)) return
+
+  size.value--
+}
+
+const isIncrementable = (v: number) => {
+  return v < nums.length
+}
+
+const isDecrementable = (v: number) => {
+  return v > 1
+}
+
 const nums = [
   13,
   0 - 3,
@@ -31,7 +51,7 @@ const execute = () => {
 
 const innerCalc = (index: number) => {
   if (index >= startIndex.value && index <= endIndex.value) {
-    return "bg-embar-500"
+    return "bg-yellow-300"
   }
 
   return index < size.value ? "bg-red-200" : "bg-blue-300"
@@ -44,15 +64,28 @@ execute()
   <h1 class="container mx-auto p-2">
     <div class="text-3xl">Kadane’s Algorithm</div>
     <div class="flex gap-2 items-center mt-4">
-      <label>current index</label>
-      <input
-        class="border border-gray-400 p-2 rounded-md select-none text-xl"
-        type="number"
+      <label>current size</label>
+      <div
+        class="
+          border border-gray-400
+          p-2
+          rounded-md
+          select-none
+          text-xl
+          h-10
+          w-10
+        "
         min="1"
         :max="nums.length"
-        v-model="size"
-        @change="execute"
-      />
+      >
+        {{ size }}
+      </div>
+      <button @click="sizeDecrement" class="h-8 w-8 text-lg border rounded-md">
+        -
+      </button>
+      <button @click="sizeIncrement" class="h-8 w-8 text-lg border rounded-md">
+        +
+      </button>
     </div>
     <div><span class="text-xl">Max</span> {{ maxSum }}</div>
     <div><span class="text-xl">Current</span> {{ sum }}</div>
@@ -61,7 +94,7 @@ execute()
       <span>[{{ startIndex }}] to [{{ endIndex }}]</span>
     </div>
 
-    <div class="flex gap-2">
+    <div class="flex gap-2 overflow-x-scroll mt-2">
       <div
         v-for="(num, index) in nums"
         :key="index"
